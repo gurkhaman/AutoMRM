3df8140bd7411207e4b819dace5f0e99bbc85392
arogyaGurkha <gurkhaman@outlook.com>
2025-02-12 06:43:24 +0000

    Change setup code to copy cifar 100 dataset to cache folder, so as to not download it everytime the container is rebuilt.

--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ apt-get install -y git
 pip install -r requirements.txt
 chown root:root `which py-spy`
 chmod u+s `which py-spy`
+
+cp -r cifar-100-python ~/.cache/
+cp cifar-100-python.tar.gz ~/.cache/
